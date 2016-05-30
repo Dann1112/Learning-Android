571742ff6d70055eadec8f733765ce52f3c2c028
Dann <manrique_jd@hotmail.com>
2016-05-30 00:13:24 -0500

    Add feature to ListView and new Activity
    ListView change color on item when pressing on anyone.
    Add new activity button

--- app/src/main/java/com/example/dann/testingandroid/ThirdActivity.java
+++ app/src/main/java/com/example/dann/testingandroid/ThirdActivity.java
@@ -1,18 +1,22 @@
 package com.example.dann.testingandroid;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
 
     private ListView list;
     private ArrayAdapter<String> myAdapter;
+    private Intent intent;
 
 
     @Override
@@ -20,9 +24,9 @@ public class ThirdActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
 
-        String[] myArray = {"A.M.", "Being There", "Summerteeth", "Yankee Hotel Foxtrot",
-                "A Ghost Is Born", "Kicking Television: Live in Chicago", "Sky Blue Sky",
-                "Wilco (The Album)", "The Whole Love", "Star Wars"};
+        String[] myArray = {"In order", "To use a", "List View", "We can use",
+                "An Array Adapter", "It is helpful", "Because it allows you",
+                "To adapt dynamically", "The content of a DB", "Into your List View"};
 
         myAdapter = new ArrayAdapter<>(this, R.layout.list_item, myArray);
         list = (ListView) findViewById(R.id.listLayout);
@@ -30,17 +34,58 @@ public class ThirdActivity extends AppCompatActivity {
 
 
         listListener();
+        nextActivityListener();
 
     }
 
     public void listListener() {
         list = (ListView) findViewById(R.id.listLayout);
+        list.setBackgroundColor(Color.WHITE);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(ThirdActivity.this, "Hello " + ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+
+                TextView op = (TextView) view;
+                ColorDrawable color = (ColorDrawable) op.getBackground();
+
+                if (color.getColor() == Color.WHITE)
+                    op.setBackgroundColor(Color.GREEN);
+
+                else if (color.getColor() == Color.GREEN)
+                    op.setBackgroundColor(Color.RED);
+
+                else if (color.getColor() == Color.RED)
+                    op.setBackgroundColor(Color.BLUE);
+
+                else if (color.getColor() == Color.BLUE)
+                    op.setBackgroundColor(Color.GRAY);
+
+                else if (color.getColor() == Color.GRAY)
+                    op.setBackgroundColor(Color.CYAN);
+
+                else if (color.getColor() == Color.CYAN)
+                    op.setBackgroundColor(Color.BLACK);
+
+                else if (color.getColor() == Color.BLACK)
+                    op.setBackgroundColor(Color.WHITE);
+
             }
         });
+    }
+
+    public void nextActivityListener() {
+        Button next = (Button) findViewById(R.id.nextActivity3);
+        intent = new Intent(this, FourthActivity.class);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 

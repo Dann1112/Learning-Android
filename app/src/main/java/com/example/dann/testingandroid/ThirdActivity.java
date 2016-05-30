f1c67553eeb1148e836becb531e67877c1119344
Dann <manrique_jd@hotmail.com>
2016-05-29 23:13:40 -0500

    Replace String Array with Array Adapter to fill List View

--- app/src/main/java/com/example/dann/testingandroid/ThirdActivity.java
+++ app/src/main/java/com/example/dann/testingandroid/ThirdActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,12 +12,21 @@ import android.widget.Toast;
 public class ThirdActivity extends AppCompatActivity {
 
     private ListView list;
+    private ArrayAdapter<String> myAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+
+        String[] myArray = {"A.M.", "Being There", "Summerteeth", "Yankee Hotel Foxtrot",
+                "A Ghost Is Born", "Kicking Television: Live in Chicago", "Sky Blue Sky",
+                "Wilco (The Album)", "The Whole Love", "Star Wars"};
+
+        myAdapter = new ArrayAdapter<>(this, R.layout.list_item, myArray);
+        list = (ListView) findViewById(R.id.listLayout);
+        list.setAdapter(myAdapter);
 
 
         listListener();
